da607d126f7f531af05e395ce22294d49c181ccb
Bishop Clark <scension@outlook.com>
2020-12-24 21:00:23 -0500

    form

--- src/main/scala/com/neomata/skelyserver/http/html/HtmlCodes.scala
+++ src/main/scala/com/neomata/skelyserver/http/html/HtmlCodes.scala
@@ -18,4 +18,33 @@ object HtmlCodes {
        |    </body>
        |</html>""".stripMargin
   }
+
+  def mainPage: String = {
+    s"""<html lang="en">
+       |    <head>
+       |        <meta name="viewport" content="width=device-width, minimum-scale=0.1">
+       |        <title>Decimoto</title>
+       |    </head>
+       |    <body>
+       |        <form>
+       |            <div>
+       |                <p>1. You're on the phone with a criminal who holds your friend hostage and will kill them in 10 minutes, you will: </p>
+       |                <p><input type="radio" name="question1" value="Stall">Stall them until help arrives; hopefully a professional will be able to consult better than I.</p>
+       |                <p><input type="radio" name="question1" value="Refuse">Refuse their demands; why should a criminal win?</p>
+       |                <p><input type="radio" name="question1" value="Stall">Give them what they ask for; my friend is probably terrified.</p>
+       |            </div>
+       |            <div>
+       |                <p>2. If a baby is power level: 1 and the strongest person is power level: 100, my power level is: </p>
+       |                <input type="number" name="question2" value="1">
+       |            </div>
+       |            <div>
+       |                <p>3. W-A-V-Y-: </p>
+       |                <p><input type="radio" name="question1" value="Stall">Stall them until help arrives; hopefully a professional will be able to consult better than I.</p>
+       |                <p><input type="radio" name="question1" value="Refuse">Refuse their demands; why should a criminal win?</p>
+       |                <p><input type="radio" name="question1" value="Stall">Give them what they ask for; my friend is probably terrified.</p>
+       |            </div>
+       |        </form>
+       |    </body>
+       |</html>""".stripMargin
+  }
 }
