753f801e0926b3cbe2fa1daeea63fcb51df3aba6
Bishop Clark <scension@outlook.com>
2020-12-25 01:40:55 -0500

    htmls

--- src/main/scala/com/neomata/skelyserver/http/html/HtmlCodes.scala
+++ src/main/scala/com/neomata/skelyserver/http/html/HtmlCodes.scala
@@ -31,7 +31,7 @@ object HtmlCodes {
        |                <p>1. You're on the phone with a criminal who holds your friend hostage and will kill them in 10 minutes, you will: </p>
        |                <p><input type="radio" name="question1" value="Stall">Stall them until help arrives; hopefully a professional will be able to consult better than I.</p>
        |                <p><input type="radio" name="question1" value="Refuse">Refuse their demands; why should a criminal win?</p>
-       |                <p><input type="radio" name="question1" value="Stall">Give them what they ask for; my friend is probably terrified.</p>
+       |                <p><input type="radio" name="question1" value="Comply">Give them what they ask for; my friend is probably terrified.</p>
        |            </div>
        |            <div>
        |                <p>2. If a baby is power level: 1 and the strongest person is power level: 100, my power level is: </p>
@@ -39,9 +39,35 @@ object HtmlCodes {
        |            </div>
        |            <div>
        |                <p>3. W-A-V-Y-: </p>
-       |                <p><input type="radio" name="question1" value="Stall">Stall them until help arrives; hopefully a professional will be able to consult better than I.</p>
-       |                <p><input type="radio" name="question1" value="Refuse">Refuse their demands; why should a criminal win?</p>
-       |                <p><input type="radio" name="question1" value="Stall">Give them what they ask for; my friend is probably terrified.</p>
+       |                <p><input type="checkbox" name="question3" value="S">S</p>
+       |                <p><input type="checkbox" name="question3" value="H">H</p>
+       |                <p><input type="checkbox" name="question3" value="U">U</p>
+       |                <p><input type="checkbox" name="question3" value="T">T</p>
+       |                <p><input type="checkbox" name="question3" value="X">X</p>
+       |            </div>
+       |            <div>
+       |                <p>4. Average mile time: </p>
+       |                <input type="time" name="question4" id="q4">
+       |            </div>
+       |            <div>
+       |                <p>5. Maximum consecutive pushups: </p>
+       |                <input type="number" name="question5" id="q5">
+       |            </div>
+       |            <div>
+       |                <p>6. Estimate 100m sprint: </p>
+       |                <input type="time" name="question6" id="q6">
+       |            </div>
+       |            <div>
+       |                <p>7. You need to train to become the ultimate incumbent fusion of serenity and fury: </p>
+       |                <p><input type="radio" name="question7" value="Stall">Stall them until help arrives; hopefully a professional will be able to consult better than I.</p>
+       |                <p><input type="radio" name="question7" value="Refuse">Refuse their demands; why should a criminal win?</p>
+       |                <p><input type="radio" name="question7" value="Comply">Give them what
+       |            </div>
+       |            <div>
+       |                <p>8. "Mafia Boss over the phone: So you da one I been hearin about...da one causing me all this trouble. I lost $3 million because of you...you killed my buddy Robby...with carbon monoxide for God sake. You're gonna pay for what you've done you hear me?" </p>
+       |                <p><input type="radio" name="question7" value="Logos">How could I have been the one to kill him, I have black hair and I'm lightskinned. Doesn't fit the profile. </p>
+       |                <p><input type="radio" name="question7" value="Pathos">Please... no. I swear I wasn't the one please!</p>
+       |                <p><input type="radio" name="question7" value="Ethos">Hm. Do what you you gotta do, but if you come after me I'm coming after you and I won't fail.</p>
        |            </div>
        |        </form>
        |    </body>
